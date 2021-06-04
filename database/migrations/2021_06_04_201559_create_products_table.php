030a80bac270129b31e6341f8c17e203c966af90
fatoumashour <ashourfatoum@gmail.com>
2021-06-04 23:31:13 +0300

    add model and migrate

---
+++ database/migrations/2021_06_04_201559_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('name_trade');
+            $table->string('price');
+            $table->string('count');
+            $table->string('size');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->text('detailes');
+            $table->string('warning_using');
+            $table->string('offers');
+            $table->bigInteger('catigory_id');
+            $table->bigInteger('brand_id');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
