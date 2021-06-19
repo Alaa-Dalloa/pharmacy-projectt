21fc08d45b420120c5a3be5c4959c1d40c9a1df3
fatoumashour <ashourfatoum@gmail.com>
2021-06-19 10:00:22 +0300

    add relations in data

--- app/Brand.php
+++ app/Brand.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    //
+   public function products (){
+
+   	return this-> hasMany('APP\Product' ,'brand_id');
+   }
+
+   
+
+    
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function catigory()
+    {
+    	return $this -> belongsTo('App\catigory' ,' catigory_id');
+    }
+    public function brand()
+    {
+    	return $this -> belongsTo('App\Brand' , 'brand_id');
+    }
+
+    public function calibers (){
+
+   	return this-> hasMany('APP\Caliber' ,'product_id');
+   }
+
+   public function images (){
+
+   	return this-> hasMany('APP\Image' ,'product_id');
+   }
 }
